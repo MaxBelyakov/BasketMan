60320d72ffc3c1b8d24d412f804a1095d337f79b
MaxBelyakov <belyakovdevelopment@gmail.com>
2021-11-02 14:27:48 +0300

    First commit

---
+++ Assets/Scripts/DieSpace.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieSpace : MonoBehaviour {
+
+    public GameObject respawn;
+    private Rigidbody2D rb2;
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Camera.main.backgroundColor = Color.red;
+            StartCoroutine(waiter(other));
+        }
+    }
+    
+    IEnumerator waiter(Collider2D other)
+    {
+        
+        yield return new WaitForSeconds(1);
+        Camera.main.backgroundColor = Color.blue;
+
+        /* NEED TO STOP ROTATION AND MOVING THERE */
+
+        other.transform.rotation = Quaternion.Euler(0, 0, 0); /* rotate player to start position */
+
+        /* turn Face to start position */
+        if (Player_controller.FaceRight != true)
+        {
+            other.transform.localScale = new Vector3(other.transform.localScale.x * -1, other.transform.localScale.y, other.transform.localScale.z);
+            Player_controller.FaceRight = !Player_controller.FaceRight;
+        }
+        
+        other.transform.position = respawn.transform.position;
+
+    }
+}
---
+++ Assets/Scripts/Player_controller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Player_controller : MonoBehaviour {
+
+    public float speed = 20f;
+    private Rigidbody2D rb;
+    public static bool FaceRight = true;
+
+    void Start () {
+        rb = GetComponent <Rigidbody2D> ();
+	}
+	
+	void Update () {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right * moveX * speed *Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector2.up * 8000);
+
+        if (moveX > 0 && !FaceRight)
+            flip();
+        else if (moveX < 0 && FaceRight)
+            flip();
+
+        
+    }
+
+    void flip ()
+    {
+        FaceRight = !FaceRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+}
