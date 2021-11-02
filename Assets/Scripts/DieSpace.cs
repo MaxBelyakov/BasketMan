101c645f2e9595e11c82daa5c928e5638e8669b2
MaxBelyakov <belyakovdevelopment@gmail.com>
2021-11-02 18:36:56 +0300

    v.1.2

--- Assets/Scripts/DieSpace.cs
+++ Assets/Scripts/DieSpace.cs
@@ -4,24 +4,22 @@ using System.Collections;
 public class DieSpace : MonoBehaviour {
 
     public GameObject respawn;
-    private Rigidbody2D rb2;
 
     void OnTriggerEnter2D (Collider2D other)
     {
         if (other.tag == "Player")
         {
+            Player_controller.notDead = 0;
             Camera.main.backgroundColor = Color.red;
+            Player_controller.rb.freezeRotation = true;
             StartCoroutine(waiter(other));
         }
     }
     
     IEnumerator waiter(Collider2D other)
     {
-        
         yield return new WaitForSeconds(1);
         Camera.main.backgroundColor = Color.blue;
-
-        /* NEED TO STOP ROTATION AND MOVING THERE */
 
         other.transform.rotation = Quaternion.Euler(0, 0, 0); /* rotate player to start position */
 
@@ -31,8 +29,9 @@ public class DieSpace : MonoBehaviour {
             other.transform.localScale = new Vector3(other.transform.localScale.x * -1, other.transform.localScale.y, other.transform.localScale.z);
             Player_controller.FaceRight = !Player_controller.FaceRight;
         }
-        
-        other.transform.position = respawn.transform.position;
 
+        Player_controller.notDead = 1;
+        other.transform.position = respawn.transform.position;
+        Player_controller.rb.freezeRotation = false;
     }
 }
--- Assets/Scripts/Player_controller.cs
+++ Assets/Scripts/Player_controller.cs
@@ -3,8 +3,9 @@
 public class Player_controller : MonoBehaviour {
 
     public float speed = 20f;
-    private Rigidbody2D rb;
+    public static Rigidbody2D rb;
     public static bool FaceRight = true;
+    public static float notDead = 1; /* the way to stop moving player after dead. Become 0 when dead */
 
     void Start () {
         rb = GetComponent <Rigidbody2D> ();
@@ -12,17 +13,15 @@ public class Player_controller : MonoBehaviour {
 	
 	void Update () {
         float moveX = Input.GetAxis("Horizontal");
-        rb.MovePosition(rb.position + Vector2.right * moveX * speed *Time.deltaTime);
+        rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime * notDead);
 
         if (Input.GetKeyDown(KeyCode.Space))
-            rb.AddForce(Vector2.up * 8000);
+            rb.AddForce(Vector2.up * 8000 * notDead);
 
         if (moveX > 0 && !FaceRight)
             flip();
         else if (moveX < 0 && FaceRight)
             flip();
-
-        /* add test line */
     }
 
     void flip ()
@@ -30,5 +29,4 @@ public class Player_controller : MonoBehaviour {
         FaceRight = !FaceRight;
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
-
 }
